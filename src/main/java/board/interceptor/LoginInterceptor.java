f86920174c776025cce3c40d012de19363c212b4
blebleli <hsj128jhs@gmail.com>
2018-08-08 00:27:47 +0900

    0808 notebook commit

--- src/main/java/board/interceptor/LoginInterceptor.java
+++ src/main/java/board/interceptor/LoginInterceptor.java
@@ -1,5 +1,7 @@
 package board.interceptor;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -8,6 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import board.board.model.BoardVo;
+import board.board.service.BoardService;
+import board.board.service.BoardServiceInf;
 import board.student.service.StudentService;
 import board.student.service.StudentServiceInf;
 
@@ -19,15 +24,17 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler
-			) throws Exception {
-			
+			HttpServletResponse response, Object handler) throws Exception {
+		
+		//로그인 상태가 아니라면 , 튕겨내려고 intercept 하는것	
 		HttpSession session = request.getSession();
+		
 		Object vo = session.getAttribute("studentVo");
 		if (vo == null) {
-			response.sendRedirect("login/login");
+			response.sendRedirect("/login/view");
 			return false;
 		}
+
 		return true;
 	}
 
